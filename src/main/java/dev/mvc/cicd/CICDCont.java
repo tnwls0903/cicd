d669e279c4491d470b83d154e22c4cd04ddd1ad6
Your Name <you@example.com>
2024-07-16 15:33:54 +0900

    CICD 테스트

--- src/main/java/dev/mvc/cicd/CICDCont.java
+++ src/main/java/dev/mvc/cicd/CICDCont.java
@@ -10,11 +10,13 @@ import lombok.extern.slf4j.Slf4j;
 @RestController
 public class CICDCont {
   // http://localhost:9091/cicd?sw=-1
-  // http://localhost:9091/cicd?sw=0
   // http://localhost:9091/cicd?sw=1
+  // http://localhost:9091/cicd?sw=0
   // http://localhost:9091/cicd?sw=2
+  // http://localhost:9091/cicd?sw=3
+  // http://3.34.236.207:9091/cicd?sw=3
   @GetMapping("/cicd")
-  public String cicd(@RequestParam(name="sw", defaultValue = "1") Integer sw) {
+  public String cicd(@RequestParam(defaultValue = "1") Integer sw) {
     String msg = "";
     
     if (sw == -1) {
@@ -29,11 +31,11 @@ public class CICDCont {
     } else if (sw == 2) {
       msg = "/cide workflow 테스트";
       log.info(msg);
-    } else if (sw == 2) {
+    } else if (sw == 3) {
       msg = "/cicd Github action + EC2 최종 테스트 1";
       log.info(msg);
     }
     
     return "<h3>" + msg + "</h3>";
-  } // <- 중괄호 빠지면 에러 발생
+  }
 }
